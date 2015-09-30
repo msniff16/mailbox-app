14f32b4df64d3f20620d56a4e12c18829fa3a42e
Matthew <matthew.sniff@climate.com>
2015-09-30 10:08:02 -0700

    interface made

---
+++ Mailbox/InboxViewController.swift
@@ -0,0 +1,21 @@
+//
+//  InboxViewController.swift
+//  Mailbox
+//
+//  Created by Matthew Sniff on 9/30/15.
+//  Copyright © 2015 Matthew Sniff. All rights reserved.
+//
+
+import UIKit
+
+class InboxViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    // view loaded
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
